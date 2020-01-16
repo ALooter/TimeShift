09756cdcb471cc9fd9205deb4c5e5762ebc32fd7
legeat <1rideriv@gmail.com>
2020-01-16 23:38:04 +0200

    Health, damage scripts

---
+++ Assets/Scripts/PlayerHealth.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerHealth : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public int maxHealth = 100;
+    public int currentHealth;
+    void Start()
+    {
+        currentHealth = maxHealth;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+        public void DamagePlayer(int Damage, Vector3 direction)
+            {
+                currentHealth -= Damage;
+            }
+}
