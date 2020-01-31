313dd16fbbe326a8fd1440f801e6ea4d9d875f09
looter-test <59710615+looter-test@users.noreply.github.com>
2020-01-31 15:09:43 +0200

    First day patch 2

---
+++ Assets/Characters/Char2/Player2AnimScript.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player2AnimScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    private Animator anim;
+
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetAxis("Horizontal2") > 0 || Input.GetAxis("Vertical2") > 0)
+        {
+            anim.SetBool("IsRunning", true);
+        }
+        else
+        {
+            anim.SetBool("IsRunning", false);
+        }
+    }
+}
